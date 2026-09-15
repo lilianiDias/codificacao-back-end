cd3ed7c2a2cd0ec6be1f0858438e827fb213f75c
liliani mendes <mendessantos0023@gmail.com>
2026-09-14 21:31:26 -0300

    feat: adiciona aula03 sobre streams e buffers com gerador de log

---
+++ aula03-streams-buffers/gerarLogGigante.js
@@ -0,0 +1,14 @@
+import fs from 'fs';
+
+const data = new Date().toISOString().split('T')[0];
+const hora = new Date().toLocaleDateString();
+
+const streamEscrita = fs.createWriteStream('servidor.log');
+console.log('Gerando arquivo de lçog simulado...');
+
+for (let i = 0; i < 400000; i++){
+    const tipo = i % 7 === 0 ? 'ERROR' : 'INFO';
+    streamEscrita.write(`[${data} - ${hora}] Linha ${i}: Status 200 - Mensagem de teste ${tipo} \n`);
+
+}
+streamEscrita.end();
