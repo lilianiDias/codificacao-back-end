c72fd9477f7f312e63ed549e7429c127ae845122
liliani mendes <mendessantos0023@gmail.com>
2026-09-18 20:16:24 -0300

    feat: adicionar arquivos

---
+++ aula04-tratamento-global-excecoes/server.js
@@ -0,0 +1,31 @@
+import express from 'express';
+
+const app = express();
+app.use(express.json());
+
+process.on('uncaughtException', (err) => {
+    console.error('[ERRO DE PROCESSO - uncaughtException]: ', err.message);
+});
+process.on('unhandledRejection', (reason) =>{
+    console.error('[PROMISE REJEITADA - uncaughtRejection]: ', reason);
+});
+  app.get('/sucesso', (req, res) => {
+    res.json({sucess: true, message: 'Operação Realizada com sucesso!'});
+  });
+
+  app.get('/erro-sincrono', (req, res, next) =>{
+    try{
+        throw new Error('Falha ao processar a Regra de Negócios !');
+    }
+      catch(erro){
+        next(erro);
+    }
+  });
+
+  app.get('/erro-assincrono', async(req, res, next) => {
+    try{
+        await Promise.reject(new ERROR('Erro na consulta no banco de dados externo'));
+    }catch(erro){
+        next(erro);
+    }
+  });
